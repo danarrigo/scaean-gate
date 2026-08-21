032f5891b48197f47054dd78db2d66e0590df843
1920xl080 <arrigo1888@gmail.com>
2026-08-21 15:02:36 +0700

    feat: implement app-b frontend

---
+++ applications/app-b/client/src/app/services/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private http = inject(HttpClient);
+  private backendUrl = 'http://localhost:8082';
+
+  private opts = { withCredentials: true };
+
+  getMe(): Observable<any> {
+    return this.http.get(`${this.backendUrl}/me`, this.opts);
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(`${this.backendUrl}/logout`, {}, this.opts);
+  }
+
+  getEvents(): Observable<any> {
+    return this.http.get(`${this.backendUrl}/events`, this.opts);
+  }
+
+  redirectToLogin() {
+    window.location.href = `${this.backendUrl}/auth/login`;
+  }
+
+  redirectToSSOLogout() {
+    window.location.href = 'http://localhost:8080/logout';
+  }
+}
